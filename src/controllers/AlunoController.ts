2e7483e21d8c5449f3b7580e2116d6dc317069db
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2021-01-16 13:56:38 -0300

    add Aluno, Blog, Service controllers

---
+++ src/controllers/AlunoController.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+
+import AlunoAllService from '../services/Aluno/AlunoAllService'
+import AlunoFindService from '../services/Aluno/AlunoFindService'
+import AlunoAddService from '../services/Aluno/AlunoAddService'
+
+export default class AlunoController {
+  public async all(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const alunoService = new AlunoAllService()
+      return res.send(await alunoService.all())
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public async findAluno(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const alunoService = new AlunoFindService()
+      return res.send(await alunoService.findAluno(req.params.id))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public async add(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const alunoService = new AlunoAddService()
+      return res.send(await alunoService.add(req.body))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+}
---
+++ src/controllers/EmpresaController.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+
+import EmpresaAllService from '../services/Empresa/EmpresaAllService'
+import EmpresaFindService from '../services/Empresa/EmpresaFindService'
+import EmpresaAddService from '../services/Empresa/EmpresaAddService'
+
+export default class EmpresaController {
+  public async all(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const empresaService = new EmpresaAllService()
+      return res.send(await empresaService.all())
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public async findEmpresa(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const empresaService = new EmpresaFindService()
+      return res.send(await empresaService.findEmpresa(req.params.id))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public async add(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const empresaService = new EmpresaAddService()
+      return res.send(await empresaService.add(req.body))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+}
