c17fdd19e1e394d66366ea1a78febcfc484ff8ce
Ricardo Fedrigo <https://RicardoFedrigo@github.com>
2021-01-16 16:46:13 -0300

    Fix BlogController

--- src/controllers/BlogController.ts
+++ src/controllers/BlogController.ts
@@ -1,35 +1,46 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-import BlogAllService from '../services/Blog/BlogAllService'
-import BlogFindService from '../services/Blog/BlogFindService'
-import BlogAddService from '../services/Blog/BlogAddService'
+import BlogAllService from "../services/Blog/BlogAllService";
+import BlogFindService from "../services/Blog/BlogFindService";
+import BlogAddService from "../services/Blog/BlogAddService";
 
 export default class BlogController {
-  public async all(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+  public async all(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | undefined> {
     try {
-      const blogService = new BlogAllService()
-      return res.send(await blogService.all())
+      const blogService = new BlogAllService();
+      return res.send(await blogService.all());
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
-  public async findBlog(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+  public async findBlog(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | undefined> {
     try {
-      const blogService = new BlogFindService()
-      return res.send(await blogService.findBlog(req.params.id))
+      const blogService = new BlogFindService();
+      return res.send(await blogService.findBlogByID(req.params.id));
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
-  public async add(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+  public async add(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | undefined> {
     try {
-      const blogService = new BlogAddService()
-      return res.send(await blogService.add(req.body))
+      const blogService = new BlogAddService();
+      return res.send(await blogService.add(req.body));
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
-
 }
