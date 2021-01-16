7e54b5b1192bb06bb0a0449e7a01099fa76ce303
Ricardo Fedrigo <https://RicardoFedrigo@github.com>
2021-01-16 14:59:14 -0300

    Fix name of function

--- src/services/Empresa/EmpresaFindService.ts
+++ src/services/Empresa/EmpresaFindService.ts
@@ -1,7 +1,7 @@
 import Empresa from "../../entities/EmpresaSchema";
 
 export default class EmpresaFindService {
-  public async find(id: string) {
+  public async findEmpresaById(id: string) {
     return await Empresa.findById(id);
   }
 }
